e9ee745a1e7d286a675cf6d28653a06fe6eb4bf9
BikrAm2003 <b2003das@gmail.com>
2022-06-23 20:29:52 +0530

    worked

---
+++ script.js
@@ -0,0 +1,98 @@
+// List of sentences
+var _CONTENT = [ 
+	"Open !!! Tap !! Go!!!", 
+	"Track your Goal", 
+	"Connect with people", 
+	"Get Fit!!!"
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed 
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#text");
+
+// Cursor element 
+var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+function Type() { 
+	// Get substring with 1 characater added
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+		// Hide the cursor
+		_CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+
+
+function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor,repeat) {
+	let paths = document.querySelectorAll("path");
+	let mode=repeat?'infinite':'forwards'
+	for (let i = 0; i < paths.length; i++) {
+		const path = paths[i];
+		const length = path.getTotalLength();
+		path.style["stroke-dashoffset"] = `${length}px`;
+		path.style["stroke-dasharray"] = `${length}px`;
+		path.style["stroke-width"] = `${strokeWidth}px`;
+		path.style["stroke"] = `${strokeColor}`;
+		path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+		path.style["animation-delay"] = `${i * delay}s`;
+	}
+}
+setTextAnimation(0.1,6,2,'ease','#fff0f0',true);
